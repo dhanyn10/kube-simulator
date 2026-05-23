57150f8b12f3e1129fa6a90cc74af0ac3a89490e
dhanyn10 <11478316+dhanyn10@users.noreply.github.com>
2026-05-23 10:03:09 +0000

    Refactor ternaries & reorganize backend

--- backend/system/system_resources_others.go
+++ backend/system/system_resources_others.go
@@ -1,12 +1,12 @@
 //go:build !windows
 
-package main
+package system
 
 import (
 	"runtime"
 )
 
-func (a *App) GetSystemResources() map[string]interface{} {
+func GetSystemResources() map[string]interface{} {
 	cores := runtime.NumCPU()
 	
 	// Mock or simple fallback for non-windows platforms in this specific project context
