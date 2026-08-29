23bf7a47eeb140effdb1f4f8d5e0e8bfbdeceb5c
dhanyn10 <11478316+dhanyn10@users.noreply.github.com>
2026-08-29 05:45:05 +0000

    refactor: clean up frontend code and use syscall for win32 resources

--- backend/system/system_resources_windows.go
+++ backend/system/system_resources_windows.go
@@ -6,8 +6,31 @@ import (
 	"runtime"
 	"strconv"
 	"strings"
+	"syscall"
+	"unsafe"
+)
 
-	"golang.org/x/sys/windows"
+type memoryStatusEx struct {
+	cbSize                  uint32
+	dwMemoryLoad            uint32
+	ullTotalPhys            uint64
+	ullAvailPhys            uint64
+	ullTotalPageFile        uint64
+	ullAvailPageFile        uint64
+	ullTotalVirtual         uint64
+	ullAvailVirtual         uint64
+	ullAvailExtendedVirtual uint64
+}
+
+type fileTime struct {
+	dwLowDateTime  uint32
+	dwHighDateTime uint32
+}
+
+var (
+	modkernel32              = syscall.NewLazyDLL("kernel32.dll")
+	procGlobalMemoryStatusEx = modkernel32.NewProc("GlobalMemoryStatusEx")
+	procGetSystemTimes       = modkernel32.NewProc("GetSystemTimes")
 )
 
 func parseValueFromOutput(out []byte) uint64 {
@@ -27,26 +50,30 @@ func parseValueFromOutput(out []byte) uint64 {
 	return 0
 }
 
-func fileTimeToUint64(ft windows.Filetime) uint64 {
-	return uint64(ft.HighDateTime)<<32 | uint64(ft.LowDateTime)
+func fileTimeToUint64(ft fileTime) uint64 {
+	return uint64(ft.dwHighDateTime)<<32 | uint64(ft.dwLowDateTime)
 }
 
 func getWinMemoryStats() (totalGB uint64, freeGB float64) {
-	var msx windows.MemoryStatusEx
-	msx.Length = uint32(windows.SizeofMemoryStatusEx)
-	err := windows.GlobalMemoryStatusEx(&msx)
-	if err == nil {
-		totalGB = (msx.TotalPhys + (1024 * 1024 * 1024) - 1) / (1024 * 1024 * 1024)
-		freeGB = float64(msx.AvailPhys) / (1024 * 1024 * 1024)
+	var msx memoryStatusEx
+	msx.cbSize = uint32(unsafe.Sizeof(msx))
+	ret, _, _ := procGlobalMemoryStatusEx.Call(uintptr(unsafe.Pointer(&msx)))
+	if ret != 0 {
+		totalGB = (msx.ullTotalPhys + (1024 * 1024 * 1024) - 1) / (1024 * 1024 * 1024)
+		freeGB = float64(msx.ullAvailPhys) / (1024 * 1024 * 1024)
 		return totalGB, freeGB
 	}
 	return 16, 8.0
 }
 
 func getWinCpuUsage() int {
-	var idleTime, kernelTime, userTime windows.Filetime
-	err := windows.GetSystemTimes(&idleTime, &kernelTime, &userTime)
-	if err == nil {
+	var idleTime, kernelTime, userTime fileTime
+	ret, _, _ := procGetSystemTimes.Call(
+		uintptr(unsafe.Pointer(&idleTime)),
+		uintptr(unsafe.Pointer(&kernelTime)),
+		uintptr(unsafe.Pointer(&userTime)),
+	)
+	if ret != 0 {
 		idle := fileTimeToUint64(idleTime)
 		kernel := fileTimeToUint64(kernelTime)
 		user := fileTimeToUint64(userTime)
@@ -59,7 +86,7 @@ func getWinCpuUsage() int {
 	return 0
 }
 
-// GetSystemResources returns the local CPU cores and total/available memory in GB using native Win32 API via golang.org/x/sys/windows
+// GetSystemResources returns the local CPU cores and total/available memory in GB using native Win32 API
 func GetSystemResources() map[string]interface{} {
 	cores := runtime.NumCPU()
 	totalMemoryGB, freeMemoryGB := getWinMemoryStats()
